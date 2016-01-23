226ec26d7646485fc860db6f218d4d14156f795b
Mauro Vieira <maurosilva.aj@gmail.com>
2016-01-23 00:32:31 -0200

    adcionado suporte mobile

---
+++ js/javascript.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+
+window.onload = function(){
+    var widthWin = window.innerHeight;
+    var txtWidth = document.querySelector('#largura');
+    var btnMenu = document.querySelector(".btn-menu-mobile");
+    var btnOn = false;
+    var subMenu = document.querySelector(".sub-menu-mobile");
+    
+    btnMenu.addEventListener("click",function(){
+        btnOn = !btnOn;
+        
+        if(btnOn){
+            subMenu.style.height = "60px";
+        }else{
+            subMenu.style.height = "0px";
+        }
+    },false);
+    
+    
+    
+    
+    
+            
+    alert("jnjkl");        
+        
+    
+    
+    
+    
+    function printWidth(){
+        widthWin = window.innerWidth;
+        txtWidth.innerHTML = "Tamanho da janela: "+widthWin;
+        
+    }
+    
+    setInterval(printWidth, 1);
+    
+}
+
